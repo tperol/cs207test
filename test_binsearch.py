d5387b06fe3cb21cd29c659fcee6830882a708a2
tperol <tperol@seas.harvard.edu>
2016-02-24 20:24:52 -0500

    implemented unit test for binsearch

---
+++ test_binsearch.py
@@ -0,0 +1,42 @@
+import unittest
+
+from binsearch import binary_search
+
+
+import unittest
+
+class MyTest(unittest.TestCase):
+
+
+    def test_binsearch(self):
+        input = list(range(10))
+        self.assertEqual(binary_search(input, 5),5)
+        self.assertEqual(binary_search(input, 4.5),-1)
+        self.assertEqual(binary_search(input, 10),-1)
+        self.assertEqual(binary_search([5], 5),0)
+
+    def test_zerol(self):
+        with self.assertRaises(ValueError):
+            binary_search([],5)
+
+    def test_nan(self):
+        with self.assertRaises(TypeError):
+            binary_search([1,float("nan"),4],5)
+
+    # def test_mymath(self):
+    #     self.assertEqual(myaverage([2,3]), 2.5)
+        
+    def test_char(self):
+        with self.assertRaises(SyntaxError):
+            binary_search([1,"blabla",4],5)
+            
+    # def test_zerol(self):
+    #     with self.assertRaises(ValueError):
+    #         myaverage([])
+
+
+
+# WHEN DOING INCREMENTAL TEST
+
+suite = unittest.TestLoader().loadTestsFromModule(MyTest())
+unittest.TextTestRunner().run(suite)
